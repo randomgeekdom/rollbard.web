4140605f6a8faaeccc9bf0d6dd639495d674ef49
randomgeekdom <randomgeekdom@gmail.com>
2022-02-04 21:29:03 -0600

    starting rollbard to tm integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './terra-machina/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'terra-machina',
+    children:[
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
